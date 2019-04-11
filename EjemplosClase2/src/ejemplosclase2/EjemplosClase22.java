f777bb7fa50ba25826a1982657afefb897c48353
Ian <iancarlosortegaleon@gmail.com>
2019-04-11 11:15:36 -0500

    buffer

---
+++ EjemplosClase2/src/ejemplosclase2/EjemplosClase22.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Salas
+ */
+public class EjemplosClase22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String nombre;
+        String apellido;
+        int edad;
+        String ciudad;
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Por favor ingrese sus nombres");
+        nombre = entrada.nextLine();
+      
+        System.out.println("Por favor ingrese sus apellidos");
+        apellido = entrada.nextLine();
+        
+        System.out.println("Por favor ingrese su edad");
+        edad = entrada.nextInt();
+        entrada.nextLine(); // limpieza de buffer de entrada de datos
+        
+        System.out.println("Por favor ingrese su ciudad");
+        ciudad = entrada.nextLine();
+        
+        System.out.println("Su nombre es: "+nombre+"\n\n\t"+"Su apellido"
+                + " es: "+apellido+ " "+edad+" "+ciudad);Ian 
+        
+    }
+    
+}
